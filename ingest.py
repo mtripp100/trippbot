699e1fd8ff4d82f80a2c1996d1198218f0436fa1
Magnus Tripp <mtripp100@gmail.com>
2017-07-04 19:22:11 +0100

    First version of ingester.

---
+++ ingest.py
@@ -0,0 +1,20 @@
+from lxml import html
+import requests
+
+def ingest_compact():
+    page = requests.get("https://en.wikipedia.org/wiki/List_of_Latin_phrases_(full)")
+    tree = html.fromstring(page.content)
+    
+    rows = tree.xpath("//table[contains(@class, 'wikitable')]/tr[position() > 1]")
+    print("{} rows downloaded.".format(len(rows)))
+
+    phrases = []
+    for row in rows:
+        latin = "".join(row[0].xpath(".//text()"))
+        translation = "".join(row[1].xpath(".//text()"))
+        notes = "".join(row[2].xpath(".//text()")) if len(row) > 2 else None
+        phrases.append((latin, translation, notes))
+
+    print("{} phrases ingested.".format(len(phrases)))
+
+ingest_compact()
