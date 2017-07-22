fb5f4545d32abaea5e4eafdafcfd3861f42f787f
Magnus Tripp <mtripp100@gmail.com>
2017-07-22 21:50:46 +0100

    First go at web interface.

--- database.py
+++ database.py
@@ -11,28 +11,32 @@ def _get_connection():
         user=url.username,
         password=url.password,
         host=url.hostname,
-        port=url.port
+        port=url.port,
     )
 
 
 _connection = _get_connection()
+_connection.autocommit = True
 
 def do_upload(phrases):
     with _connection.cursor() as cursor:
         psycopg2.extras.execute_values(cursor,
                                        "INSERT INTO phrases(latin, translation, notes) "
                                        "VALUES %s", phrases)
-        _connection.commit()
 
 def pick_phrase():
     with _connection.cursor() as cursor:
         cursor.execute("SELECT phrase_id, latin, translation, notes "
                        "FROM phrases ORDER BY RANDOM() LIMIT 1")
-        _connection.commit()
         return cursor.fetchone()
 
 def record_phrase(tweet_id, phrase_id):
     with _connection.cursor() as cursor:
         cursor.execute("INSERT INTO tweets(tweet_id, phrase_id) "
                        "VALUES (%s, %s)", (tweet_id, phrase_id))
-        _connection.commit()
+
+def get_phrase(phrase_id):
+    with _connection.cursor() as cursor:
+        cursor.execute("SELECT latin, translation, notes "
+                       "FROM phrases WHERE phrase_id=%(phrase_id)s", {"phrase_id": phrase_id})
+        return cursor.fetchone()
