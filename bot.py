6dfe66ba35cb5e434a2b246a83d041b339e5909a
Magnus Tripp <mtripp100@gmail.com>
2017-08-19 13:13:49 +0100

    Don't bother checking in.

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 from database import pick_phrase, record_phrase
-from datetime import datetime
+import datetime
 import os
 import tweepy
 import click
-import requests
 
 @click.command()
 @click.option("--interval", default=4, help="The script is invoked once an hour, so only tweet "
@@ -11,13 +10,11 @@ import requests
               type=click.IntRange(1, 24))
 @click.option("--force", is_flag=True)
 def run(interval, force):
-    now = datetime.utcnow()
+    now = datetime.datetime.utcnow()
     if should_tweet(now, interval) or force:
         send_tweet()
     else:
         print("Nothing to do...")
-
-    check_in()
 
 def should_tweet(dt, interval):
     return ((dt.time().hour % interval) == 0)
@@ -37,14 +34,10 @@ def build_url(phrase_id):
     return "https://trippbot.herokuapp.com/q/{}".format(phrase_id)
 
 def get_api():
-    auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
-    auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_SECRET'])
+    auth = tweepy.OAuthHandler(os.environ["CONSUMER_KEY"], os.environ["CONSUMER_SECRET"])
+    auth.set_access_token(os.environ["ACCESS_TOKEN"], os.environ["ACCESS_SECRET"])
 
     return tweepy.API(auth)
-
-def check_in():
-    res = requests.get("https://nosnch.in/d744f3c018")
-    print(res.status_code, res.text)
 
 
 if __name__ == "__main__":
